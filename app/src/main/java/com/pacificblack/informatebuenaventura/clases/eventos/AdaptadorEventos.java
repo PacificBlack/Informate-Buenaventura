0b82e4f3e65fa4591b1538a6617182a62b059fdf
pacificbalck2@gmail.com <pacificbalck2@gmail.com>
2020-01-08 20:30:53 -0500

    Creacion de clases

---
+++ app/src/main/java/com/pacificblack/informatebuenaventura/clases/eventos/AdaptadorEventos.java
@@ -0,0 +1,69 @@
+package com.pacificblack.informatebuenaventura.clases.eventos;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pacificblack.informatebuenaventura.R;
+
+import java.util.ArrayList;
+
+public class AdaptadorEventos extends RecyclerView.Adapter<AdaptadorEventos.EventosHolder> {
+
+
+    ArrayList<Eventos> listaEventos;
+
+    public AdaptadorEventos(ArrayList<Eventos> listaEventos) {
+        this.listaEventos = listaEventos;
+    }
+
+    @NonNull
+    @Override
+    public EventosHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_eventos,null,false);
+
+        return new EventosHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventosHolder holder, int position) {
+
+        holder.tituloeventos.setText(listaEventos.get(position).getLugar_row_eventos());
+        holder.descripcioneventos.setText(listaEventos.get(position).getDescripcion_row_eventos());
+        holder.fechapublicacioneventos.setText(listaEventos.get(position).getFechapublicacion_row_eventos());
+        holder.lugareventos.setText(listaEventos.get(position).getLugar_row_eventos());
+        holder.vistaeventos.setText(String.valueOf(listaEventos.get(position).getVistas_eventos()));
+        holder.imageneventos.setImageResource(listaEventos.get(position).getImagen1_eventos());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaEventos.size();
+    }
+
+    public class EventosHolder extends RecyclerView.ViewHolder {
+
+        TextView tituloeventos,descripcioneventos,fechapublicacioneventos,lugareventos,vistaeventos;
+        ImageView imageneventos;
+
+        public EventosHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tituloeventos = itemView.findViewById(R.id.titulo_row_eventos);
+            descripcioneventos = itemView.findViewById(R.id.descripcion_row_eventos);
+            fechapublicacioneventos = itemView.findViewById(R.id.fecha_row_eventos);
+            lugareventos = itemView.findViewById(R.id.lugar_row_eventos);
+            vistaeventos = itemView.findViewById(R.id.vista_row_eventos);
+            imageneventos = itemView.findViewById(R.id.imagen_row_eventos);
+
+        }
+    }
+}
--- app/src/main/java/com/pacificblack/informatebuenaventura/fragments/eventos/EventosFragment.java
+++ app/src/main/java/com/pacificblack/informatebuenaventura/fragments/eventos/EventosFragment.java
@@ -4,17 +4,28 @@ package com.pacificblack.informatebuenaventura.fragments.eventos;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pacificblack.informatebuenaventura.R;
+import com.pacificblack.informatebuenaventura.clases.eventos.AdaptadorEventos;
+import com.pacificblack.informatebuenaventura.clases.eventos.Eventos;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventosFragment extends Fragment {
+
+    RecyclerView recyclerEventos;
+
+    ArrayList<Eventos> listaEventos;
+
 
 
     public EventosFragment() {
@@ -25,8 +36,30 @@ public class EventosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eventos, container, false);
+View vista = inflater.inflate(R.layout.fragment_eventos, container, false);
+
+
+        listaEventos = new ArrayList<>();
+        recyclerEventos = vista.findViewById(R.id.recycler_eventos);
+        recyclerEventos.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        llenarlista_eventos();
+
+        AdaptadorEventos adaptadorEventos = new AdaptadorEventos(listaEventos);
+        recyclerEventos.setAdapter(adaptadorEventos);
+
+return vista;
+    }
+
+    private void llenarlista_eventos() {
+
+        listaEventos.add(new Eventos("Bienvenidos al perreo sondiacal",
+                "Clasifsafojsmewf osdfkfe wyo no lsoe las esdf",
+                "Otro texto bien chilin",
+                "Yo ni se mi parcerito crack",
+                R.drawable.imagencita,
+                2599));
+
     }
 
 }
